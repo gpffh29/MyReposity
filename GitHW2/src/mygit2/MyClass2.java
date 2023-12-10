504fe18b335e8999d5371338362eb55873632a67
PC <PC@DESKTOP-SHTMEJR>
2023-12-10 17:33:24 +0900

    Change in master branch

--- GitHW2/src/mygit2/MyClass2.java
+++ GitHW2/src/mygit2/MyClass2.java
@@ -4,6 +4,7 @@ public class MyClass2 {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
 		System.out.println("Second test");
+		System.out.println("My changed in master branch");
 	}
 
 }
