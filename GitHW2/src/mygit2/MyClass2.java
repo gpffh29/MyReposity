4dd13968dc84597a551517ad0090845956cdee5f
PC <PC@DESKTOP-SHTMEJR>
2023-12-10 17:30:03 +0900

    First commit in dev branch

--- GitHW2/src/mygit2/MyClass2.java
+++ GitHW2/src/mygit2/MyClass2.java
@@ -4,6 +4,8 @@ public class MyClass2 {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
 		System.out.println("Second test");
+		YourClass your = new YourClass();
+		your.sayHello("Everyone");
 	}
 
 }
